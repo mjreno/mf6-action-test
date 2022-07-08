38436099c100c7faf5a745a012d3af082d5add3e
mjreno <mjreno@ncsu.edu>
2022-07-08 15:35:16 -0400

    add fprettify cfg file

--- .github/common/fformat-check.sh
+++ .github/common/fformat-check.sh
@@ -5,15 +5,17 @@ SEARCHPATHS=(src)
 FCHECKFAILS=()
 
 for path in "${SEARCHPATHS[@]}"
+echo "path=$path"
 do
     mapfile -d '' files < <(find "$path" -type f -print0 | grep -z '\.[fF][0-9+]')
     for file in "${files[@]}"
     do
-        if [[ ! -z $(fprettify -d -c ../modflow6/distribution/.fprettify.yaml "$file" 2>/dev/null) ]]; then
+        if [[ ! -z $(fprettify -d -c distribution/.fprettify.yaml "$file" 2>/dev/null) ]]; then
             FCHECKFAILS+=("$file")
         fi
     done
 done
+echo "file checks complete"
 
 if [[ ${#FCHECKFAILS[@]} > 0 ]]; then
     echo "Files failing formatting check:"
