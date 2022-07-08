26ba034921aad214cc68b643c256267dd75bf5a6
mjreno <mjreno@ncsu.edu>
2022-07-08 16:12:08 -0400

    add excludes to script

--- .github/common/fformat-check.sh
+++ .github/common/fformat-check.sh
@@ -4,27 +4,34 @@
 SEARCHPATHS=(src)
 FCHECKFAILS=()
 
+EXCLUDEDIRS=(src/Utilities/Libraries/blas
+             src/Utilities/Libraries/daglib
+             src/Utilities/Libraries/rcm
+             src/Utilities/Libraries/sparsekit
+             src/Utilities/Libraries/sparskit2)
+EXCLUDEFILES=(src/Utilities/InputOutput.f90)
+
 for path in "${SEARCHPATHS[@]}"
 do
-    echo "path=$path"
     mapfile -d '' files < <(find "$path" -type f -print0 | grep -z '\.[fF][0-9+]')
     for file in "${files[@]}"
     do
-        if [[ ! -z $(fprettify -d -c distribution/.fprettify.yaml "$file" 2>/dev/null) ]]; then
+        if [[ ! "${EXCLUDEDIRS[*]}" =~ $(dirname "$file") ]] &&
+           [[ ! "${EXCLUDEFILES[*]}" =~ "${file}" ]] &&
+           [[ ! -z $(fprettify -d -c distribution/.fprettify.yaml "$file" 2>/dev/null) ]]; then
             FCHECKFAILS+=("$file")
         fi
     done
 done
-echo "file checks complete"
 
 if [[ ${#FCHECKFAILS[@]} > 0 ]]; then
-    echo "Files failing formatting check:"
+    echo -e "\n\nFiles failing formatting check:\n"
     for f in "${FCHECKFAILS[@]}"
     do
         echo "$f"
     done
     echo -e "\nTo verify file format diff in local environment run:"
-    echo "  'fprettify -d -c <path to modflow6>/distribution/.fprettify.yaml <filepath>'"
+    echo -e "  'fprettify -d -c <path to modflow6>/distribution/.fprettify.yaml <filepath>'\n\n"
     exit 1
 fi
 
