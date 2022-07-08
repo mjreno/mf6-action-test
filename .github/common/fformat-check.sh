fbb27384cdfa0d531d06bceec3da2f776374b254
mjreno <mjreno@ncsu.edu>
2022-07-08 18:31:42 -0400

    cleanup

--- .github/common/fformat-check.sh
+++ .github/common/fformat-check.sh
@@ -13,26 +13,26 @@ EXCLUDEFILES=(src/Utilities/InputOutput.f90)
 
 for path in "${SEARCHPATHS[@]}"
 do
-    mapfile -d '' files < <(find "$path" -type f -print0 | grep -z '\.[fF][0-9+]')
+    mapfile -d '' files < <(find "${path}" -type f -print0 | grep -z '\.[fF][0-9+]')
     for file in "${files[@]}"
     do
-        found=0
-        for p in "${EXCLUDEDIRS[@]}"; do [[ "$p" == $(dirname "$file") ]] && found=1 && break; done
-        if [[ $found == 1 ]]; then continue; fi
-        for f in "${EXCLUDEFILES[@]}"; do [[ "$f" == "$file" ]] && found=1 && break; done
-        if [[ $found == 1 ]]; then continue; fi
+        exclude=0
+        for p in "${EXCLUDEDIRS[@]}"; do [[ "${p}" == $(dirname "${file}") ]] && exclude=1 && break; done
+        if [[ ${exclude} == 1 ]]; then continue; fi
+        for f in "${EXCLUDEFILES[@]}"; do [[ "${f}" == "${file}" ]] && exclude=1 && break; done
+        if [[ ${exclude} == 1 ]]; then continue; fi
 
-        if [[ ! -z $(fprettify -d -c distribution/.fprettify.yaml "$file" 2>/dev/null) ]]; then
+        if [[ ! -z $(fprettify -d -c distribution/.fprettify.yaml "${file}" 2>/dev/null) ]]; then
             FCHECKFAILS+=("$file")
         fi
     done
 done
 
 if [[ ${#FCHECKFAILS[@]} > 0 ]]; then
-    echo -e "\n\nFiles failing formatting check:\n"
+    echo -e "\nFiles failing formatting check:\n"
     for f in "${FCHECKFAILS[@]}"
     do
-        echo "$f"
+        echo "${f}"
     done
     echo -e "\nTo verify file format diff in local environment run:"
     echo -e "  'fprettify -d -c <path to modflow6>/distribution/.fprettify.yaml <filepath>'\n\n"
