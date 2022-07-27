faf79e26416421c2f056904e7583b3bca6c034fb
mjreno <mjreno@ncsu.edu>
2022-07-27 16:02:33 -0400

    test

--- autotest2/conftest.py
+++ autotest2/conftest.py
@@ -4,11 +4,11 @@ from modflow_devtools import (
     MFTestContext,
 )
 
-modflow6_testbin = "../bin"
+modflow6_devbin = "../bin"
 
 def pytest_sessionstart(session):
-    MFTestContext(testbin=modflow6_testbin)
+    MFTestContext(testbin=modflow6_devbin)
 
 @pytest.fixture(scope="session")
 def mf6testctx(request):
-    return MFTestContext(testbin=modflow6_testbin)
+    return MFTestContext(testbin=modflow6_devbin)
