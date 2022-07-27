29463b8eb3e1554cea046597f6294d4715e07284
mjreno <mjreno@ncsu.edu>
2022-07-26 21:15:43 -0400

    test

--- autotest2/conftest.py
+++ autotest2/conftest.py
@@ -1,10 +1,14 @@
 import pytest
-#from filelock import FileLock
 
 from modflow_devtools import (
     MFTestContext,
 )
 
+modflow6_testbin = "../bin"
+
+def pytest_sessionstart(session):
+    MFTestContext(testbin=modflow6_testbin)
+
 @pytest.fixture(scope="session")
 def mf6testctx(request):
-    return MFTestContext(testbin="../bin")
+    return MFTestContext(testbin=modflow6_testbin)
